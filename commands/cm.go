f65d7c37e915e6464b1785fd4935bf3864ba866a
aram price <aprice@pivotal.io>
2016-05-10 12:25:35 -0700

    Set default API url
    Signed-off-by: Can Berk Güder <cbguder@pivotal.io>

--- commands/cm.go
+++ commands/cm.go
@@ -1,7 +1,7 @@
 package commands
 
 type CMCommand struct {
-	ApiURL string `long:"api" description:"Credential Manager API URL"`
+	ApiURL string `long:"api" description:"Credential Manager API URL" default:"https://pivotal-credential-manager.cfapps.io/"`
 
 	Set SetCommand `command:"set" description:"Set a secret value"`
 }
