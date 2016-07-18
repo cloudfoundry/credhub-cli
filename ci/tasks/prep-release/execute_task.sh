cf86ae299648b75842bd5b1cad7bcedfb0789b5b
Stu Pollock <spollock@pivotal.io>
2016-07-18 15:33:11 -0700

    See if we can use the time-resource
    instead of date in our task.sh
    
    [#126160547] Update pipeline to use bosh-init deployment for acceptance tests
    
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotal.io>

--- ci/tasks/prep-release/execute_task.sh
+++ ci/tasks/prep-release/execute_task.sh
@@ -2,14 +2,16 @@
 
 set -exu
 
+PREP_RELEASE_INPUT=$(mktemp -d -t prep-release-input)
+echo "$(date +%s)" > $PREP_RELEASE_INPUT/input
+
 PREP_RELEASE_OUTPUT=$(mktemp -d -t prep-release-output)
 
 fly \
   -t private \
   execute \
   -c task.yml \
+  -i clock=$PREP_RELEASE_INPUT \
   -i code-repo=../../.. \
   -i task-repo=../../.. \
   -o prep-release-output=$PREP_RELEASE_OUTPUT
-
-# rm -rf $PREP_RELEASE_OUTPUT
--- ci/tasks/prep-release/task.sh
+++ ci/tasks/prep-release/task.sh
@@ -2,12 +2,14 @@
 
 set -eux
 
+ls -l clock
+
 export GOPATH=$PWD/go
 export GOARCH=amd64
 BUILD_ROOT=$PWD
 
 binary_name="cm-cli"
-build_number=$(date +%s)
+build_number=$(cat clock/input)
 
 echo ${binary_name} > ${PREP_RELEASE_OUTPUT_PATH}/name
 echo ${build_number} > ${PREP_RELEASE_OUTPUT_PATH}/tag
