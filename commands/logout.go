b626b948cacaa1721e3aeef2c0f4619731ba06d4
Philip Kuryloski <pkuryloski@pivotal.io>
2016-08-22 08:31:11 -0700

    Also revoke the token with the UAA
    when login fails and tokens are present
    
    [finishes #126432339] Any existing user login session should be discarded if user attempts to log in again

--- commands/logout.go
+++ commands/logout.go
@@ -13,18 +13,24 @@ type LogoutCommand struct {
 
 func (cmd LogoutCommand) Execute([]string) error {
 	cfg, err := config.ReadConfig()
-	if err != nil {
-		cfg = config.Config{}
-	} else {
-		if cfg.RefreshToken != "" && cfg.RefreshToken != "revoked" {
-			authRepository := repositories.NewAuthRepository(client.NewHttpClient(cfg.AuthURL), false)
-			authRepository.SendRequest(client.NewTokenRevocationRequest(cfg), "logout")
-		}
+	if err == nil {
+		SendLogoutIfNecessary(cfg)
 	}
 
+	RevokeConfig(cfg)
+	fmt.Println("Logout Successful")
+	return nil
+}
+
+func SendLogoutIfNecessary(cfg config.Config) {
+	if cfg.RefreshToken != "" && cfg.RefreshToken != "revoked" {
+		authRepository := repositories.NewAuthRepository(client.NewHttpClient(cfg.AuthURL), false)
+		authRepository.SendRequest(client.NewTokenRevocationRequest(cfg), "logout")
+	}
+}
+
+func RevokeConfig(cfg config.Config) {
 	cfg.AccessToken = "revoked"
 	cfg.RefreshToken = "revoked"
 	config.WriteConfig(cfg)
-	fmt.Println("Logout Successful")
-	return nil
 }
