e3eeebe8e18dff8fa87aaa70346203e9563900c6
Iryna Shustava <ishustava@pivotal.io>
2016-09-15 15:15:43 -0700

    Install go and git in windows unit tests task
    [#119567329] Investigate how to automate Windows Worker VM
    
    Signed-off-by: Larry Hamel <lhamel@pivotal.io>

--- ci/tasks/unit-tests-windows/task.bat
+++ ci/tasks/unit-tests-windows/task.bat
@@ -1,9 +1,34 @@
+powershell -command set-executionpolicy remotesigned
+
+set work_dir = "C:\concourse"
+set download_dir = "C:\downloads"
+
+md %download_dir% -Force
+
+set git_url = "https://github.com/git-for-windows/git/releases/download/v2.9.0.windows.1/Git-2.9.0-64-bit.exe"
+set git_path = "%download_dir%\Git-2.9.0-64-bit.exe"
+
+set go_url = "https://storage.googleapis.com/golang/go1.6.2.windows-amd64.msi"
+set go_path = "%download_dir%\go1.6.2.windows-amd64.msi"
+
+set wc = New-Object System.Net.WebClient
+
+Write-Output "Downloading Git..."
+%wc%.DownloadFile($git_url, $git_path)
+
+Write-Output "Downloading Go..."
+%wc%.DownloadFile(%go_url%, %go_path%)
+
+%git_path% /SP- /SILENT
+
+msiexec /passive /i $go_path
+
+md %work_dir% -Force
+
 SET GOPATH=%CD%\go
 SET PATH=C:\Go\bin;%GOPATH%\bin;C:\Program Files\Git\cmd;%PATH%
 
 cd %GOPATH%\src\github.com\pivotal-cf\credhub-cli
-
-powershell -command set-executionpolicy remotesigned
 
 go get github.com/onsi/ginkgo/ginkgo
 go get -v -t ./...
