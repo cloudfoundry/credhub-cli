4152774c39ecea8813bc10c04fbfd96a817cd386
Sebastian Wittenkamp <swittenkamp@pivotal.io>
2017-01-04 15:21:48 -0800

    Utility for generating a test certificate

---
+++ generate_test_certs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Generate a default CA and Certificate
+
+TIMESTAMP=$(date +%s)
+CERTNAME="certificate-$TIMESTAMP"
+TMPFILE="/tmp/credhub_test_cert_$TIMESTAMP.json"
+CERTFILE="./tmp/$CERTNAME.cer"
+
+mkdir -p tmp
+
+./build/credhub ca-generate -n default --common-name root 2>&1 > /dev/null
+./build/credhub generate -n $CERTNAME -t certificate --common-name $CERTNAME --ca default -e code_signing -e email_protection 2>&1 > /dev/null
+./build/credhub get -n $CERTNAME --output-json > $TMPFILE
+
+printf "$(cat $TMPFILE | jq '.certificate')" | sed -e 's/"//g' >  $CERTFILE
+printf "$(cat $TMPFILE | jq '.private_key')" | sed -e 's/"//g' >> $CERTFILE
+
+echo "Generated $CERTFILE"
