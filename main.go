ddec09b7fb1d6762921024a1bd7460653618a7f6
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-05-25 11:23:48 -0700

    Display help if no command or invalid command is provided
    [#145393601] Calling credhub CLI without commands should display help
    
    Signed-off-by: Edie Beer <ebeer@pivotal.io>

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ import (
 func main() {
 	debug.SetTraceback("all")
 	parser := flags.NewParser(&commands.CredHub, flags.HelpFlag)
+	parser.SubcommandsOptional = true
+	parser.CommandHandler = func(command flags.Commander, args []string) error {
+		if command == nil {
+			parser.WriteHelp(os.Stderr)
+			os.Exit(1)
+		}
+
+		return command.Execute(args)
+	}
 
 	_, err := parser.Parse()
 	if err != nil {
