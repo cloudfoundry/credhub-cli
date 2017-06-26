b1b0eb00918bd06b4e0d2a64c3249e191103f935
Phil Goodwin <pgoodwin@pivotal.io>
2017-06-26 11:20:25 -0400

    Token endpoint returns latest auth token
    [#147718835] credhub --token should fetch or refresh the token before displaying it
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- commands/token.go
+++ commands/token.go
@@ -4,13 +4,30 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cloudfoundry-incubator/credhub-cli/client"
 	"github.com/cloudfoundry-incubator/credhub-cli/config"
+	"github.com/cloudfoundry-incubator/credhub-cli/models"
+	"github.com/cloudfoundry-incubator/credhub-cli/repositories"
 )
 
 func init() {
 	CredHub.Token = func() {
 		cfg := config.ReadConfig()
 		if cfg.AccessToken != "" {
+
+			refresh_request := client.NewRefreshTokenRequest(cfg)
+			repository := repositories.NewAuthRepository(client.NewHttpClient(cfg), true)
+			refreshed_token, err := repository.SendRequest(refresh_request, "")
+
+			if err != nil {
+				fmt.Println("Bearer " + cfg.AccessToken)
+			}
+
+			cfg.AccessToken = refreshed_token.(models.Token).AccessToken
+			cfg.RefreshToken = refreshed_token.(models.Token).RefreshToken
+
+			config.WriteConfig(cfg)
+
 			fmt.Println("Bearer " + cfg.AccessToken)
 		} else {
 			fmt.Fprint(os.Stderr, "You are not logged in to a CredHub server")
