d29099b23ed3779089cad9bd6f79469bcaee2d83
Andrew Leung <aleung@pivotal.io>
2017-07-17 15:29:29 -0400

    Refactor the import command to call a simplified set api
    Signed-off-by: Tanzeeb Khalili <tkhalili@pivotal.io>

--- api/set.go
+++ api/set.go
@@ -1,106 +1,27 @@
 package api
 
 import (
-	"io/ioutil"
-	"net/http"
-
 	"github.com/cloudfoundry-incubator/credhub-cli/actions"
 	"github.com/cloudfoundry-incubator/credhub-cli/client"
 	"github.com/cloudfoundry-incubator/credhub-cli/config"
-	"github.com/cloudfoundry-incubator/credhub-cli/errors"
 	"github.com/cloudfoundry-incubator/credhub-cli/models"
 	"github.com/cloudfoundry-incubator/credhub-cli/repositories"
 )
 
-func Set(
-	credentialIdentifier string,
-	credentialType string,
-	noOverwrite bool,
-	value string,
-	caName string,
-	root string,
-	certificate string,
-	private string,
-	public string,
-	rootString string,
-	certificateString string,
-	privateString string,
-	publicString string,
-	username string,
-	password string,
-) (models.CredentialResponse, error) {
-
+func Set(credentialType string, name string, value interface{}, overwrite bool) (models.CredentialResponse, error) {
 	cfg := config.ReadConfig()
+
 	repository := repositories.NewCredentialRepository(client.NewHttpClient(cfg))
 
 	action := actions.NewAction(repository, &cfg)
-	var request *http.Request
-	if credentialType == "ssh" || credentialType == "rsa" {
-		var err error
 
-		err = setStringFieldFromFile(&public, &publicString)
-		if err != nil {
-			return models.CredentialResponse{}, err
-		}
+	request := client.NewSetCredentialRequest(cfg, credentialType, name, value, overwrite)
 
-		err = setStringFieldFromFile(&private, &privateString)
-		if err != nil {
-			return models.CredentialResponse{}, err
-		}
+	result, err := action.DoAction(request, name)
 
-		request = client.NewSetRsaSshRequest(cfg, credentialIdentifier, credentialType, publicString, privateString, !noOverwrite)
-	} else if credentialType == "certificate" {
-		var err error
-
-		err = setStringFieldFromFile(&root, &rootString)
-		if err != nil {
-			return models.CredentialResponse{}, err
-		}
-
-		err = setStringFieldFromFile(&certificate, &certificateString)
-		if err != nil {
-			return models.CredentialResponse{}, err
-		}
-
-		err = setStringFieldFromFile(&private, &privateString)
-		if err != nil {
-			return models.CredentialResponse{}, err
-		}
-
-		request = client.NewSetCertificateRequest(cfg, credentialIdentifier, rootString, caName, certificateString, privateString, !noOverwrite)
-	} else if credentialType == "user" {
-		request = client.NewSetUserRequest(cfg, credentialIdentifier, username, password, !noOverwrite)
-	} else if credentialType == "password" {
-		request = client.NewSetCredentialRequest(cfg, credentialType, credentialIdentifier, password, !noOverwrite)
-	} else if credentialType == "json" {
-		request = client.NewSetJsonCredentialRequest(cfg, credentialType, credentialIdentifier, value, !noOverwrite)
-	} else {
-		request = client.NewSetCredentialRequest(cfg, credentialType, credentialIdentifier, value, !noOverwrite)
-	}
-
-	credential, err := action.DoAction(request, credentialIdentifier)
-
-	return credential.(models.CredentialResponse), err
-}
-
-func setStringFieldFromFile(fileField, stringField *string) error {
-	var err error
-	if *fileField != "" {
-		if *stringField != "" {
-			return errors.NewCombinationOfParametersError()
-		}
-		*stringField, err = readFile(*fileField)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func readFile(filename string) (string, error) {
-	dat, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return "", errors.NewFileLoadError()
+		return models.CredentialResponse{}, err
 	}
-	return string(dat), nil
+
+	return result.(models.CredentialResponse), err
 }
--- commands/import.go
+++ commands/import.go
@@ -2,40 +2,72 @@ package commands
 
 import (
 	"fmt"
-	"reflect"
+
+	"net/http"
 
 	"os"
+
+	"reflect"
 
 	"github.com/cloudfoundry-incubator/credhub-cli/api"
 	"github.com/cloudfoundry-incubator/credhub-cli/errors"
 	"github.com/cloudfoundry-incubator/credhub-cli/models"
+	"github.com/cloudfoundry-incubator/credhub-cli/repositories"
 )
 
 type ImportCommand struct {
 	File string `short:"f" long:"file" description:"File containing credentials to import. File must be in yaml format containing a list of credentials under the key 'credentials'. Name, type and value are required for each credential in the list." required:"true"`
 }
 
-func (cmd ImportCommand) Execute([]string) error {
+var (
+	err        error
+	repository repositories.Repository
+	bulkImport models.CredentialBulkImport
+	request    *http.Request
+)
 
-	results, err := api.Import(cmd.File)
+func (cmd ImportCommand) Execute([]string) error {
+	err = bulkImport.ReadFile(cmd.File)
+
 	if err != nil {
-		if isAuthenticationError(err) {
-			fmt.Println(err)
-		}
 		return err
 	}
 
-	for i, result := range results {
-		if result.Err != nil {
-			fmt.Fprintf(os.Stderr, "Credential '%s' at index %d could not be set: %v\n", result.Name, i, result.Err)
-		} else {
-			models.Println(result.Cred, false)
-		}
-	}
+	setCredentials(bulkImport)
 
 	return nil
 }
 
+func setCredentials(bulkImport models.CredentialBulkImport) {
+	var name string
+
+	for i, credential := range bulkImport.Credentials {
+
+		switch credentialName := credential["name"].(type) {
+		case string:
+			name = credentialName
+		default:
+			name = ""
+		}
+
+		credentialType := credential["type"].(string)
+		value := credential["value"]
+		overwrite := credential["overwrite"].(bool)
+
+		result, err := api.Set(credentialType, name, value, overwrite)
+
+		if err != nil {
+			if isAuthenticationError(err) {
+				fmt.Println(err)
+				break
+			}
+			fmt.Fprintf(os.Stderr, "Credential '%s' at index %d could not be set: %v\n", name, i, err)
+			continue
+		}
+
+		models.Println(result, false)
+	}
+}
 func isAuthenticationError(err error) bool {
 	return reflect.DeepEqual(err, errors.NewNoApiUrlSetError()) ||
 		reflect.DeepEqual(err, errors.NewRevokedTokenError()) ||
