f66385f44a47067358260be752be9691337d86ba
Rebecca Chin <rchin@pivotal.io>
2017-08-09 11:24:42 -0400

    Implement FindByPath
    Signed-off-by: Andrew Leung <aleung@pivotal.io>

--- credhub/find.go
+++ credhub/find.go
@@ -1,6 +1,12 @@
 package credhub
 
-import "github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials"
+)
 
 // Retrieves a list of stored credential names which contain the search.
 func (ch *CredHub) FindByPartialName(nameLike string) ([]credentials.Base, error) {
@@ -9,7 +15,24 @@ func (ch *CredHub) FindByPartialName(nameLike string) ([]credentials.Base, error
 
 // Retrieves a list of stored credential names which are within the specified path.
 func (ch *CredHub) FindByPath(path string) ([]credentials.Base, error) {
-	panic("Not implemented")
+	var creds map[string][]credentials.Base
+
+	resp, err := ch.Request(http.MethodGet, "/api/v1/data?path="+path, nil)
+
+	if err != nil {
+		return []credentials.Base{}, err
+	}
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &creds)
+
+	if err != nil {
+		return []credentials.Base{}, err
+	}
+
+	return creds["credentials"], nil
 }
 
 // Retrieves a list of all paths which contain credentials.
---
+++ credhub/find_test.go
@@ -0,0 +1,108 @@
+package credhub_test
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	. "github.com/cloudfoundry-incubator/credhub-cli/credhub"
+	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials"
+	"github.com/cloudfoundry-incubator/credhub-cli/credhub/server"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Find", func() {
+	Describe("FindByPath()", func() {
+		var dummy DummyAuth
+		var serv server.Server
+		var creds []credentials.Base
+		var err error
+
+		BeforeEach(func() {
+			serv = server.Server{
+				ApiUrl:             "http://example.com",
+				InsecureSkipVerify: true,
+			}
+		})
+
+		It("requests credentials for a specified path", func() {
+			dummy = DummyAuth{Response: &http.Response{
+				Body: ioutil.NopCloser(bytes.NewBufferString("")),
+			}}
+
+			ch := CredHub{
+				Server: &serv,
+				Auth:   &dummy,
+			}
+			creds, err = ch.FindByPath("/some/example/path")
+			urlPath := dummy.Request.URL.Path
+			Expect(urlPath).To(Equal("/api/v1/data?path=/some/example/path"))
+		})
+
+		Context("when successful", func() {
+			It("returns a list of stored credential names which are within the specified path", func() {
+				expectedResponse := `{
+  "credentials": [
+    {
+      "version_created_at": "2017-05-09T21:09:26Z",
+      "name": "/some/example/path/example-cred-0"
+    },
+    {
+      "version_created_at": "2017-05-09T21:09:07Z",
+      "name": "/some/example/path/example-cred-1"
+    }
+  ]
+}`
+				dummy = DummyAuth{Response: &http.Response{
+					Body: ioutil.NopCloser(bytes.NewBufferString(expectedResponse)),
+				}}
+
+				ch := CredHub{
+					Server: &serv,
+					Auth:   &dummy,
+				}
+				creds, err = ch.FindByPath("/some/example/path")
+
+				Expect(creds[0].Name).To(Equal("/some/example/path/example-cred-0"))
+				Expect(creds[0].VersionCreatedAt).To(Equal("2017-05-09T21:09:26Z"))
+				Expect(creds[1].Name).To(Equal("/some/example/path/example-cred-1"))
+				Expect(creds[1].VersionCreatedAt).To(Equal("2017-05-09T21:09:07Z"))
+
+			})
+		})
+
+		Context("when request fails", func() {
+			It("returns an error", func() {
+				dummy = DummyAuth{Error: errors.New("Network error occurred")}
+
+				ch := CredHub{
+					Server: &serv,
+					Auth:   &dummy,
+				}
+
+				creds, err = ch.FindByPath("/some/example/path")
+
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("when response body cannot be unmarshalled", func() {
+			It("returns an error", func() {
+				dummy = DummyAuth{Response: &http.Response{
+					Body: ioutil.NopCloser(bytes.NewBufferString("something-invalid")),
+				}}
+
+				ch := CredHub{
+					Server: &serv,
+					Auth:   &dummy,
+				}
+				creds, err = ch.FindByPath("/some/example/path")
+
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+})
