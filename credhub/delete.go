0eb678d1e33a4534924f935f4a285b9329baf6fd
Rebecca Chin <rchin@pivotal.io>
2017-08-15 14:43:40 -0400

    Implement delete
    Signed-off-by: Andrew Leung <aleung@pivotal.io>

--- credhub/delete.go
+++ credhub/delete.go
@@ -1,6 +1,19 @@
 package credhub
 
+import (
+	"net/http"
+	"net/url"
+)
+
 // Deletes a credential by name
 func (ch *CredHub) Delete(name string) error {
-	panic("Not implemented")
+	query := url.Values{}
+	query.Set("name", name)
+	resp, err := ch.Request(http.MethodDelete, "/api/v1/data", query, nil)
+
+	if err == nil {
+		defer resp.Body.Close()
+	}
+
+	return err
 }
