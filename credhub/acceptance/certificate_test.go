0a94514d78285d726466f311ba993532ffbcc847
Rebecca Chin <rchin@pivotal.io>
2017-08-16 10:11:34 -0400

    Add acceptance for find by path.
    Use the same path prefix for all credentials generated in a given test run
    
    Signed-off-by: Andrew Leung <aleung@pivotal.io>

--- credhub/acceptance/certificate_test.go
+++ credhub/acceptance/certificate_test.go
@@ -1,32 +1,17 @@
 package acceptance_test
 
 import (
-	"fmt"
-	"time"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	. "github.com/cloudfoundry-incubator/credhub-cli/credhub"
-	"github.com/cloudfoundry-incubator/credhub-cli/credhub/auth/uaa"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/generate"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/values"
 )
 
 var _ = Describe("Certificate", func() {
-	var ch *CredHub
-
-	BeforeEach(func() {
-		var err error
-		ch, err = New("https://localhost:9000",
-			SkipTLSValidation(),
-			AuthBuilder(uaa.PasswordGrantBuilder("credhub_cli", "", "credhub", "password")))
-
-		Expect(err).ToNot(HaveOccurred())
-	})
-
 	Specify("certificate lifecycle", func() {
-		name := fmt.Sprintf("/acceptance/certificate/%v", time.Now().UnixNano())
+		name := testCredentialPath("some-certificate")
+
 		generateCert := generate.Certificate{
 			CommonName: "example.com",
 			SelfSign:   true,
@@ -39,41 +24,41 @@ var _ = Describe("Certificate", func() {
 		}
 
 		By("generate a certificate with path " + name)
-		certificate, err := ch.GenerateCertificate(name, generateCert, false)
+		certificate, err := credhubClient.GenerateCertificate(name, generateCert, false)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(certificate.Value.Certificate).ToNot(BeEmpty())
 		Expect(certificate.Value.PrivateKey).ToNot(BeEmpty())
 		firstGeneratedCertificate := certificate.Value
 
 		By("generate the certificate again without overwrite returns same certificate")
-		certificate, err = ch.GenerateCertificate(name, generateCert, false)
+		certificate, err = credhubClient.GenerateCertificate(name, generateCert, false)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(certificate.Value).To(Equal(firstGeneratedCertificate))
 
 		By("setting the certificate again without overwrite returns same certificate")
-		certificate, err = ch.SetCertificate(name, setCert, false)
+		certificate, err = credhubClient.SetCertificate(name, setCert, false)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(certificate.Value).To(Equal(firstGeneratedCertificate))
 
 		By("overwriting the certificate with generate")
-		certificate, err = ch.GenerateCertificate(name, generateCert, true)
+		certificate, err = credhubClient.GenerateCertificate(name, generateCert, true)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(certificate.Value).ToNot(Equal(firstGeneratedCertificate))
 
 		By("overwriting the certificate with set")
-		certificate, err = ch.SetCertificate(name, setCert, true)
+		certificate, err = credhubClient.SetCertificate(name, setCert, true)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(certificate.Value).To(Equal(setCert))
 
 		By("getting the certificate")
-		certificate, err = ch.GetCertificate(name)
+		certificate, err = credhubClient.GetCertificate(name)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(certificate.Value).To(Equal(setCert))
 
 		By("deleting the certificate")
-		err = ch.Delete(name)
+		err = credhubClient.Delete(name)
 		Expect(err).ToNot(HaveOccurred())
-		_, err = ch.GetCertificate(name)
+		_, err = credhubClient.GetCertificate(name)
 		Expect(err).To(HaveOccurred())
 	})
 })
--- credhub/acceptance/password_test.go
+++ credhub/acceptance/password_test.go
@@ -1,71 +1,54 @@
 package acceptance_test
 
 import (
-	"fmt"
-	"time"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	. "github.com/cloudfoundry-incubator/credhub-cli/credhub"
-	"github.com/cloudfoundry-incubator/credhub-cli/credhub/auth/uaa"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/generate"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/values"
 )
 
 var _ = Describe("Password", func() {
-	var ch *CredHub
-
-	BeforeEach(func() {
-		var err error
-		ch, err = New("https://localhost:9000",
-			SkipTLSValidation(),
-			AuthBuilder(uaa.PasswordGrantBuilder("credhub_cli", "", "credhub", "password")))
-
-		Expect(err).ToNot(HaveOccurred())
-	})
-
 	Specify("password lifecycle", func() {
-
-		name := fmt.Sprintf("/acceptance/password/%v", time.Now().UnixNano())
+		name := testCredentialPath("some-password")
 		generatePassword := generate.Password{Length: 10}
 
 		By("generate a password with path " + name)
-		password, err := ch.GeneratePassword(name, generatePassword, false)
+		password, err := credhubClient.GeneratePassword(name, generatePassword, false)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(password.Value).To(HaveLen(10))
 		firstGeneratedPassword := password.Value
 
 		By("generate the password again without overwrite returns same password")
-		password, err = ch.GeneratePassword(name, generatePassword, false)
+		password, err = credhubClient.GeneratePassword(name, generatePassword, false)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(password.Value).To(Equal(firstGeneratedPassword))
 
 		By("setting the password again without overwrite returns same password")
-		password, err = ch.SetPassword(name, values.Password("some-password"), false)
+		password, err = credhubClient.SetPassword(name, values.Password("some-password"), false)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(password.Value).To(Equal(firstGeneratedPassword))
 
 		By("overwriting the password with generate")
-		password, err = ch.GeneratePassword(name, generatePassword, true)
+		password, err = credhubClient.GeneratePassword(name, generatePassword, true)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(password.Value).To(HaveLen(10))
 		Expect(password.Value).ToNot(Equal(firstGeneratedPassword))
 
 		By("overwriting the password with set")
-		password, err = ch.SetPassword(name, values.Password("some-password"), true)
+		password, err = credhubClient.SetPassword(name, values.Password("some-password"), true)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(password.Value).To(BeEquivalentTo("some-password"))
 
 		By("getting the password")
-		password, err = ch.GetPassword(name)
+		password, err = credhubClient.GetPassword(name)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(password.Value).To(BeEquivalentTo("some-password"))
 
 		By("deleting the password")
-		err = ch.Delete(name)
+		err = credhubClient.Delete(name)
 		Expect(err).ToNot(HaveOccurred())
-		_, err = ch.GetPassword(name)
+		_, err = credhubClient.GetPassword(name)
 		Expect(err).To(HaveOccurred())
 	})
 })
