15a2b097fccabbbbade4ac06a41167d3fc9bcdf5
Tanzeeb Khalili <tkhalili@pivotal.io>
2017-08-25 09:53:33 -0400

    Update examples
    Signed-off-by: Tanzeeb Khalili <tkhalili@pivotal.io>

--- credhub/auth/example_test.go
+++ credhub/auth/example_test.go
@@ -8,19 +8,26 @@ import (
 )
 
 func ExampleOAuth() {
-	return
+	_ = func() {
+		// To retrieve the access token from the CredHub client, use type assertion
+		ch, err := credhub.New(
+			"http://example.com",
+			credhub.Auth(auth.UaaPassword("client-id", "client-secret", "username", "password")),
+		)
+		if err != nil {
+			panic("couldn't connect to credhub")
+		}
 
-	// To retrieve the access token from the CredHub client, use type assertion
-	ch := credhub.CredHub{}
-	oauth, ok := ch.Auth.(*auth.OAuthStrategy)
-	if !ok {
-		panic("Not using UAA")
+		oauth, ok := ch.Auth.(*auth.OAuthStrategy)
+		if !ok {
+			panic("Not using UAA")
+		}
+
+		fmt.Println("Before logging out: ", oauth.AccessToken())
+		oauth.Logout()
+		fmt.Println("After logging out: ", oauth.AccessToken())
+		// Sample Output:
+		// Before logging out: some-access-token
+		// After logging out:
 	}
-
-	fmt.Println("Before logging out: ", oauth.AccessToken())
-	oauth.Logout()
-	fmt.Println("After logging out: ", oauth.AccessToken())
-	// FIXME Output:
-	// Before logging out: some-access-token
-	// After logging out:
 }
