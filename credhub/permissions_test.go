08dc844e098c5eb958cdc230021702120414614d
Gareth Smith <gsmith@pivotal.io>
2017-10-31 16:52:38 +0000

    AddPermissions no longer inspects the response body
    We got confused. We thought from reading
    https://credhub-api.cfapps.io/#add-permissions that AddPermissions
    should return the Permissions objects corresponding to the body returned
    by the server in the example JSON. However, when testing with a real
    credhub locally (version 1.6.1), we find that the response body is
    always empty.
    
    Signed-off-by: Jonathan Tsang <jtsang@pivotal.io>

--- credhub/permissions_test.go
+++ credhub/permissions_test.go
@@ -1,8 +1,6 @@
 package credhub_test
 
 import (
-	"errors"
-
 	. "github.com/cloudfoundry-incubator/credhub-cli/credhub"
 
 	"bytes"
@@ -17,29 +15,13 @@ import (
 var _ = Describe("AddPermissions", func() {
 	Context("when a credential exists", func() {
 		It("can add permissions to it", func() {
-			responseJSON := `{
-				  "credential_name": "/example-password",
-				  "permissions": [
-					{
-					  "actor": "some-actor",
-					  "operations": [
-						"operation-1",
-						"operation-2"
-					  ]
-					},{
-					  "actor": "already-existing-actor",
-					  "operations": ["existing-operation"]
-					}
-				  ]
-				}`
-
 			dummy := &DummyAuth{Response: &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       ioutil.NopCloser(bytes.NewBufferString(responseJSON)),
+				StatusCode: http.StatusCreated,
+				Body:       ioutil.NopCloser(bytes.NewBufferString("")),
 			}}
 			ch, _ := New("https://example.com", Auth(dummy.Builder()), ServerVersion("1.2.3"))
 
-			actualPermissions, err := ch.AddPermissions("/example-password", []permissions.Permission{
+			_, err := ch.AddPermissions("/example-password", []permissions.Permission{
 				{
 					Actor:      "some-actor",
 					Operations: []string{"operation-1", "operation-2"},
@@ -47,11 +29,6 @@ var _ = Describe("AddPermissions", func() {
 			})
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(actualPermissions).To(HaveLen(2))
-			Expect(actualPermissions[0].Actor).To(Equal("some-actor"))
-			Expect(actualPermissions[0].Operations).To(Equal([]string{"operation-1", "operation-2"}))
-			Expect(actualPermissions[1].Actor).To(Equal("already-existing-actor"))
-			Expect(actualPermissions[1].Operations).To(Equal([]string{"existing-operation"}))
 
 			By("calling the right endpoints")
 			url := dummy.Request.URL.String()
@@ -90,50 +67,4 @@ var _ = Describe("AddPermissions", func() {
 			Expect(err).To(MatchError(ContainSubstring("The request could not be completed because the credential does not exist or you do not have sufficient authorization.")))
 		})
 	})
-
-	Context("when we can't read the response body", func() {
-		It("wraps the reader error", func() {
-			dummy := &DummyAuth{Response: &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       ioutil.NopCloser(DeadReader{}),
-			}}
-			ch, _ := New("https://example.com", Auth(dummy.Builder()), ServerVersion("1.2.3"))
-
-			_, err := ch.AddPermissions("/example-password", []permissions.Permission{
-				{
-					Actor:      "some-actor",
-					Operations: []string{"operation-1", "operation-2"},
-				},
-			})
-
-			Expect(err).To(MatchError(ContainSubstring("cannot read response body in AddPermissions:")))
-		})
-	})
-
-	Context("when the json response is invalid", func() {
-		It("wraps the json error", func() {
-			responseJSON := `{`
-
-			dummy := &DummyAuth{Response: &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       ioutil.NopCloser(bytes.NewBufferString(responseJSON)),
-			}}
-			ch, _ := New("https://example.com", Auth(dummy.Builder()), ServerVersion("1.2.3"))
-
-			_, err := ch.AddPermissions("/example-password", []permissions.Permission{
-				{
-					Actor:      "some-actor",
-					Operations: []string{"operation-1", "operation-2"},
-				},
-			})
-
-			Expect(err).To(MatchError(ContainSubstring("cannot unmarshal JSON in AddPermissions:")))
-		})
-	})
 })
-
-type DeadReader struct{}
-
-func (_ DeadReader) Read(_ []byte) (int, error) {
-	return 0, errors.New("Injected error")
-}
