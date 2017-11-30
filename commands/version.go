7ac2b7a6dad63e3a1360b0ee0fc65eafd2e34778
Danny Sullivan <dsullivan@pivotal.io>
2017-11-30 11:39:26 -0500

    Do not display server version to user if they are not authenticated
    [#153194527] credhub cli refuses to fetch server version unless authenticated
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- commands/version.go
+++ commands/version.go
@@ -6,19 +6,29 @@ import (
 	"os"
 
 	"github.com/cloudfoundry-incubator/credhub-cli/config"
+	"github.com/cloudfoundry-incubator/credhub-cli/errors"
 	"github.com/cloudfoundry-incubator/credhub-cli/version"
 )
 
 func PrintVersion() error {
 	cfg := config.ReadConfig()
 
-	credHubServerVersion := "Not Found"
-	credhubInfo, err := GetApiInfo(cfg.ApiURL, cfg.CaCerts, cfg.InsecureSkipVerify)
-	if err == nil {
-		credHubServerVersion = credhubInfo.App.Version
+	credHubServerVersion := "Not Found. Have you targeted and authenticated against a CredHub server?"
+	fmt.Println("CLI Version:", version.Version)
+
+	credhubClient, err := initializeCredhubClient(cfg)
+
+	if err == nil || err.Error() != errors.NewRevokedTokenError().Error() {
+		_, err := credhubClient.FindAllPaths()
+
+		if err == nil {
+			credhubInfo, err := GetApiInfo(cfg.ApiURL, cfg.CaCerts, cfg.InsecureSkipVerify)
+			if err == nil {
+				credHubServerVersion = credhubInfo.App.Version
+			}
+		}
 	}
 
-	fmt.Println("CLI Version:", version.Version)
 	fmt.Println("Server Version:", credHubServerVersion)
 
 	return nil
--- commands/version_test.go
+++ commands/version_test.go
@@ -3,6 +3,7 @@ package commands_test
 import (
 	"net/http"
 
+	"github.com/cloudfoundry-incubator/credhub-cli/config"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
@@ -10,7 +11,53 @@ import (
 )
 
 var _ = Describe("Version", func() {
-	Context("when the request succeeds", func() {
+	Context("when the user is authenticated", func() {
+		BeforeEach(func() {
+			login()
+
+			server.RouteToHandler("GET", "/api/v1/data",
+				RespondWith(http.StatusOK, `{"data": []}`),
+			)
+		})
+
+		Context("when the request succeeds", func() {
+			BeforeEach(func() {
+				responseJson := `{"app":{"name":"CredHub","version":"0.2.0"}}`
+
+				server.RouteToHandler("GET", "/info",
+					RespondWith(http.StatusOK, responseJson),
+				)
+			})
+
+			It("displays the version with --version", func() {
+				session := runCommand("--version")
+
+				Eventually(session).Should(Exit(0))
+				sout := string(session.Out.Contents())
+				testVersion(sout)
+				Expect(sout).To(ContainSubstring("Server Version: 0.2.0"))
+			})
+		})
+
+		Context("when the request fails", func() {
+			BeforeEach(func() {
+				server.RouteToHandler("GET", "/info",
+					RespondWith(http.StatusNotFound, ""),
+				)
+			})
+
+			It("displays the version with --version", func() {
+				session := runCommand("--version")
+
+				Eventually(session).Should(Exit(0))
+				sout := string(session.Out.Contents())
+				testVersion(sout)
+				Expect(sout).To(ContainSubstring("Server Version: Not Found"))
+			})
+		})
+	})
+
+	Context("when the user is logged out", func() {
 		BeforeEach(func() {
 			responseJson := `{"app":{"name":"CredHub","version":"0.2.0"}}`
 
@@ -19,33 +66,43 @@ var _ = Describe("Version", func() {
 			)
 		})
 
-		It("displays the version with --version", func() {
+		It("returns the CLI version but not the server version", func() {
 			session := runCommand("--version")
 
 			Eventually(session).Should(Exit(0))
 			sout := string(session.Out.Contents())
 			testVersion(sout)
-			Expect(sout).To(ContainSubstring("Server Version: 0.2.0"))
+			Expect(sout).To(ContainSubstring("Server Version: Not Found. Have you targeted and authenticated against a CredHub server?"))
 		})
 	})
 
-	Context("when the request fails", func() {
+	Context("when the config contains invalid tokens", func() {
 		BeforeEach(func() {
+			responseJson := `{"app":{"name":"CredHub","version":"0.2.0"}}`
+
 			server.RouteToHandler("GET", "/info",
-				RespondWith(http.StatusNotFound, ""),
+				RespondWith(http.StatusOK, responseJson),
 			)
+
+			server.RouteToHandler("GET", "/api/v1/data",
+				RespondWith(http.StatusUnauthorized, ""),
+			)
+
+			cfg := config.ReadConfig()
+			cfg.RefreshToken = "foo"
+			cfg.AccessToken = "bar"
+			config.WriteConfig(cfg)
 		})
 
-		It("displays the version with --version", func() {
+		It("returns the CLI version but not the server version", func() {
 			session := runCommand("--version")
 
 			Eventually(session).Should(Exit(0))
 			sout := string(session.Out.Contents())
 			testVersion(sout)
-			Expect(sout).To(ContainSubstring("Server Version: Not Found"))
+			Expect(sout).To(ContainSubstring("Server Version: Not Found. Have you targeted and authenticated against a CredHub server?"))
 		})
 	})
-
 })
 
 func testVersion(sout string) {
