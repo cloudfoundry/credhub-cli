4ee0c7f775888f336a97e4e2c88d95dfe930137f
Tom Kennedy <tkennedy@pivotal.io>
2019-03-13 13:42:04 -0400

    remove xterm from ship-it
    Signed-off-by: Frances Cohen <fcohen@pivotal.io>

--- scripts/ship_it.sh
+++ scripts/ship_it.sh
@@ -33,16 +33,8 @@ function check_for_local_server(){
    if curl -s https://localhost:9000/health --insecure > /dev/null; then
       echo "Found locally running CredHub"
    else
-      echo "CredHub is not running, attempting to start"
-      pushd /Users/pivotal/workspace/credhub-release/src/credhub
-        xterm -e ./scripts/start_server.sh &
-        COUNTER=1
-        while ! curl -s https://localhost:9000/health --insecure > /dev/null; do
-          sleep 10
-          echo "Connection attempt #" ${COUNTER}
-          COUNTER=$((COUNTER + 1))
-        done;
-      popd
+      echo "CredHub is not running, please run the server and try again"
+      exit
    fi
    login_to_local_credhub
 }
