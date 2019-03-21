48ecd638be2c6131bb506cc708e52b859404e915
Victoria Henry <vhenry@pivotal.io>
2019-03-21 08:49:07 -0400

    reset

--- commands/get_permission.go
+++ commands/get_permission.go
@@ -1,9 +1,5 @@
 package commands
 
-import (
-	"fmt"
-)
-
 type GetPermissionCommand struct {
 	Actor      string `short:"a" long:"actor" required:"yes" description:"Name of the actor to grant permissions for"`
 	Path       string `short:"p" long:"path" required:"yes" description:"Name of path to grant permissions for"`
@@ -11,22 +7,6 @@ type GetPermissionCommand struct {
 	ClientCommand
 }
 
-func (c *GetPermissionCommand) GetPermission() error {
-	permission, err := c.client.GetPermissionByPathActor(c.Path, c.Actor)
-	if err != nil {
-		return err
-	}
-
-	formatOutput(c.OutputJSON, permission)
-	return err
-}
-
 func (c *GetPermissionCommand) Execute([]string) error {
-	serverVersion, _ := c.client.ServerVersion()
-	isOlderVersion := serverVersion.Segments()[0] < 2
-	if isOlderVersion {
-		return fmt.Errorf("credhub server version <2.0 not supported")
-	}
-	return c.GetPermission()
-
+	return nil
 }
--- commands/set_permission.go
+++ commands/set_permission.go
@@ -1,10 +1,5 @@
 package commands
 
-import (
-	"fmt"
-	"strings"
-)
-
 type SetPermissionCommand struct {
 	Actor      string `short:"a" long:"actor" required:"yes" description:"Name of the actor to grant permissions for"`
 	Path       string `short:"p" long:"path" required:"yes" description:"Name of path to grant permissions for"`
@@ -13,54 +8,6 @@ type SetPermissionCommand struct {
 	ClientCommand
 }
 
-func ParseOperations(operations string) []string {
-	ops := strings.Split(operations, ",")
-	trimmedOps := make([]string, len(ops))
-	for i, v := range ops {
-		trimmedOps[i] = strings.TrimSpace(v)
-	}
-	return trimmedOps
-}
-
-func (c *SetPermissionCommand) addPermission() error {
-	ops := ParseOperations(c.Operations)
-	permission, err := c.client.AddPermission(c.Path, c.Actor, ops)
-
-	if err != nil {
-		return err
-	}
-	formatOutput(c.OutputJSON, permission)
-	return nil
-}
-
-func (c *SetPermissionCommand) updatePermission(uuid string) error {
-	ops := ParseOperations(c.Operations)
-	permission, err := c.client.UpdatePermission(uuid, c.Path, c.Actor, ops)
-	if err != nil {
-		return err
-	}
-	formatOutput(c.OutputJSON, permission)
-	return nil
-}
-
 func (c *SetPermissionCommand) Execute([]string) error {
-	serverVersion, _ := c.client.ServerVersion()
-	isOlderVersion := serverVersion.Segments()[0] < 2
-	if isOlderVersion {
-		return fmt.Errorf("credhub server version <2.0 not supported")
-	}
-
-	permission, err := c.client.GetPermissionByPathActor(c.Path, c.Actor)
-	notFoundError := "The request could not be completed because the permission does not exist or you do not have sufficient authorization."
-
-	if err != nil {
-		if err.Error() == notFoundError {
-			return c.addPermission()
-		} else {
-			return err
-		}
-	} else {
-		return c.updatePermission(permission.UUID)
-	}
-
+	return nil
 }
