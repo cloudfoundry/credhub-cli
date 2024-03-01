f717d6605413333766e365d3d4a0486088ae2cf8
Geoff Franks <geoff.franks@broadcom.com>
2024-03-01 14:36:30 +0000

    ignore deprecation warning for tlsconfig.RootCAs.Subjects() since we use it for things not in systemcertpool

--- credhub/client_test.go
+++ credhub/client_test.go
@@ -118,6 +118,7 @@ var _ = Describe("Client()", func() {
 
 			Expect(tlsConfig.InsecureSkipVerify).To(BeFalse())
 			Expect(tlsConfig.PreferServerCipherSuites).To(BeTrue())
+			//lint:ignore SA1019 - ignoring tlsCert.RootCAs.Subjects is deprecated ERR because cert does not come from SystemCertPool.
 			Expect(tlsConfig.RootCAs.Subjects()).To(ConsistOf(expectedRootCAs.Subjects()))
 		})
 	})
